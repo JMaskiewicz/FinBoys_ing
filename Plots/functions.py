0644a2870b2c0a3c7041ffff0166a21f388fa003
jmaskiew <109626411+jmaskiew@users.noreply.github.com>
2024-03-28 12:09:49 +0100

    ROC to plots

---
+++ Plots/functions.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import auc
+
+def plot_roc_curve(fpr, tpr, title='ROC Curve'):
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
